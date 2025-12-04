c8bf628076c8a2b45dcc25a2cee7484da4fe8f43
Santiago <santiago.gabarre-claveria@etu.iut-tlse3.fr>
2025-12-05 00:08:31 +0100

    chat'bruti

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from flask_sqlalchemy import SQLAlchemy
 import os
 
