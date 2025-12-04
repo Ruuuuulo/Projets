a656f4dbed22270718bbda370b9cd1dedb768664
Santiago <santiago.gabarre-claveria@etu.iut-tlse3.fr>
2025-12-04 20:42:23 +0100

    bd

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+# Récupère l'URL depuis les variables d'environnement
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# Table Utilisateur
+class Utilisateur(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    mdp = db.Column(db.String(255), nullable=False)
+
+# Crée les tables si elles n'existent pas
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
